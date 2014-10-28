8ddf89e16bf510482d96f612cd5bd348cf3dd5cf
Julián <jdtorregrosas@unal.edu.co>
2014-10-27 21:56:27 -0500

    Cambios en el método llenarDia de tal forma que se llenen bien los días sin repetir materias

--- Salon.py
+++ Salon.py
@@ -20,6 +20,7 @@ class Materia:
         self.cantidadEstudiantes = cantidadEstudiantes
         self.nombre = nombre
         self.horasSemanales = horasSemanales
+        self.ultimoDia = -1
          
 class Dia:
     def __init__(self, listaMaterias,j):    
@@ -36,14 +37,14 @@ class Dia:
             print ("Jueves")
         elif self.nombre==5:
             print ("Viernes")
-        for i in range(1, len(self.listaMaterias)):
+        for i in range(len(self.listaMaterias)):
             print (self.listaMaterias[i].nombre)
          
 class Horario:
     def __init__(self, listaDias):
         self.listaDias = listaDias
     def printHorario(self):
-        for i in range(0,len(self.listaDias)):
+        for i in range(len(self.listaDias)):
             self.listaDias[i].printDia()
 
 #LLENAR LAS MATERIAS CON UN NOMBRE (Nï¿½MERO ENTERO) Y HORAS SEMANALES (4 o 6 de forma aleatoria)
@@ -53,25 +54,29 @@ def llenarMaterias(numMaterias):
         cantidadEstudiantes = 0
         if j<30:
             cantidadEstudiantes = random.gauss(35, 5)
-        elif j>=30 & j<60:
+        elif j>=30 and j<60:
             cantidadEstudiantes = random.gauss(30,5)
         else:
             cantidadEstudiantes = random.gauss(80, 15)
         horasSem = 4
         materias.append(Materia(j, horasSem, cantidadEstudiantes))
+#     for i in range(len(materias)):
+#         print(materias[i].nombre)
     return materias
 #Mï¿½todo que crea una lista de 6 materias    
 def llenarDia(materias, dia, numMaterias):
     listaMaterias=[]
-    for i in range (1,8):
-        matAleat=randint(1, len(materias))
-        #print(matAleat-1)
-        if materias[matAleat-1].horasSemanales > 0:
-            listaMaterias.append(materias[matAleat-1])
-            materias[matAleat-1].horasSemanales=materias[matAleat-1].horasSemanales-2
-        else:            
-            matAleat= randint(1, numMaterias)    
-            i = i - 1
+    for i in range (6):
+        if  len(materias)>0:
+            matAleat=randint(1, len(materias))
+            #print(materias[matAleat-1].nombre, " horas sem ",materias[matAleat-1].horasSemanales)
+            if materias[matAleat-1].horasSemanales > 0 and materias[matAleat-1].ultimoDia!= materias[matAleat-1].ultimoDia-1 and materias[matAleat-1].ultimoDia!=dia:
+                materias[matAleat-1].ultimoDia = dia
+                materias[matAleat-1].horasSemanales=materias[matAleat-1].horasSemanales-2
+                if materias[matAleat-1].horasSemanales >0:
+                    listaMaterias.append(materias[matAleat-1])
+                else:
+                    listaMaterias.append(materias.pop(matAleat-1))
     return Dia(listaMaterias,dia)
 def crearHorario(materias, numMaterias):
     dias = []
@@ -86,9 +91,9 @@ def crearCalendario(materias, numSalones, numMaterias):
     for i in range (numSalones):
         if i<=2:
             capacidad = 35
-        elif i>2 & i <=4 :#2 salones con capacidad de 40 estudiantes
+        elif i>2 and i <=4 :#2 salones con capacidad de 40 estudiantes
             capacidad = 40
-        elif i>4 & i<=6:#2 auditorios con capacidad de 100 estudiantes
+        elif i>4 and i<=6:#2 auditorios con capacidad de 100 estudiantes
             capacidad = 100
         else:
             capacidad = random.randint(20,40)
@@ -113,8 +118,9 @@ def funcionObjetivoporCalendario(calendario):
                 horasTotales = horasTotales + calendario[i].horario.listaDias[j].listaMaterias[k].horasSemanales
                 deltaCapacidades = deltaCapacidades + abs(calendario[i].capacidad - calendario[i].horario.listaDias[j].listaMaterias[k].cantidadEstudiantes)
                 temp= temp+1
-    deltaCapacidades= deltaCapacidades/temp
+    # deltaCapacidades= deltaCapacidades/temp
     z= deltaCapacidades + horasTotales
+    # print(horasTotales)
     return z
     
 def cruzarCalendarios(calendarioA, calendarioB):
@@ -140,17 +146,27 @@ def mutacion(calendario):
     salonAleatorio = random.randint(0,len(calendario)-1)
     diaAleatorio = random.randint(0,len(calendario[salonAleatorio].horario.listaDias)-1)
     materiaAleatoria = random.randint(0,len(calendario[salonAleatorio].horario.listaDias[diaAleatorio].listaMaterias)-1)
-    calendario[salonAleatorio].horario.listaDias[diaAleatorio].listaMaterias[materiaAleatoria].cantidadEstudiantes = random.randint(30, 100)
+    calendario[salonAleatorio].horario.listaDias[diaAleatorio].listaMaterias[materiaAleatoria].cantidadEstudiantes = random.randint(30, 80)
     
-numSalones = 6
-numMaterias = 90
-calendarioA = crearCalendario(llenarMaterias(numMaterias), numSalones, numMaterias)
-calendarioB = crearCalendario(llenarMaterias(numMaterias), numSalones, numMaterias)
+def buscarMejor(values):
+    temp = values[0]
+    indice = 0
+    for i in range(len(values)):
+        if values[i]<temp:
+            temp = values[i]
+            indice =  i
+    return indice
+    
+numSalones = 1 #6
+numMaterias = 30 #90
+generaciones =3
+indiceMutacion = 0.1
+calendariosFinales= []
+values = []
+calendarios = []
 # OPERADOR DE CRUCE !!!!!
 #cruzarCalendarios(calendarioA, calendarioB)
 #mutacion(calendarioA)
-values = []
-calendarios = []
 for i in range(10):
     calendarioA = crearCalendario(llenarMaterias(numMaterias), numSalones, numMaterias)
     calendarios.append(calendarioA)
@@ -160,18 +176,7 @@ for i in range(10):
 #print(chosen)
 #imprimirCalendario(calendarios[chosen[2]])
 
-generaciones = 20000
-indiceMutacion = 0.00001
-calendariosFinales= []
 
-def buscarMejor(values):
-    temp = values[0]
-    indice = 0
-    for i in range(len(values)):
-        if values[i]<temp:
-            temp = values[i]
-            indice =  i
-    return indice
 for i in range(generaciones):
     print(values)
     calendario1 = calendarios.pop(buscarMejor(values))
@@ -194,13 +199,13 @@ for i in range(generaciones):
     values= []
     for i in range(len(calendariosNuevos)):
         values.append(funcionObjetivoporCalendario(calendariosNuevos[i]))
-    #values.append(funcionObjetivoporCalendario(calendario21))
+#     values.append(funcionObjetivoporCalendario(calendario21))
 #     calendariosNuevos.append(calendario11)
 #     calendariosNuevos.append(calendario21)
-#     for i in range(8):
-#         calendarioAleatorio = crearCalendario(llenarMaterias(numMaterias), numSalones, numMaterias)
-#         calendariosNuevos.append(calendarioAleatorio)
-#         values.append(funcionObjetivoporCalendario(calendarioAleatorio))
+    for i in range(8):
+        calendarioAleatorio = crearCalendario(llenarMaterias(numMaterias), numSalones, numMaterias)
+        calendariosNuevos.append(calendarioAleatorio)
+        values.append(funcionObjetivoporCalendario(calendarioAleatorio))
     calendarios = calendariosNuevos
     for i in range (10):          
         probMutacion = random.random()
@@ -208,7 +213,7 @@ for i in range(generaciones):
             mutacion(calendarios[i])
         #values[i] = funcionObjetivoporCalendario(calendarios[i])
 for i in range (10):
-    #print calendarios[i].horario.listaDias[0].listaMaterias[0].cantidadEstudiantes, "<----- longitud"          
+#     print(calendarios[i].horario.listaDias[0].listaMaterias[0].cantidadEstudiantes, "<----- longitud")          
     imprimirCalendario(calendarios[i], i+1)
 print(values)
     
